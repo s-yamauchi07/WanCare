ff84ca056e8fffcb2274c41d8e79f1e36a191aa0
s-yamauchi07 <hand1nhand306@gmail.com>
2024-10-27 10:15:28 +0900

    Label要素のコンポーネント化

---
+++ src/app/_components/Label.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React from "react"
+
+interface LabelProps {
+  id: string
+}
+
+const Label: React.FC<LabelProps> = ({id}) => {
+  return(
+    <label 
+      className="block text-primary text-sm font-bold mb-2"
+      id={id}
+    >
+    {id}
+  </label>
+  )
+}
+export default Label;
