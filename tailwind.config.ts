fea2a8d163a4f10679d5c2b42bdd1b6460b28c5e
s-yamauchi07 <hand1nhand306@gmail.com>
2024-10-31 18:35:25 +0900

    tailwindcss-iconsの導入

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,8 @@
 import type { Config } from "tailwindcss";
+import {
+  iconsPlugin,
+  getIconCollections,
+} from "@egoist/tailwindcss-icons";
 
 const config: Config = {
   content: [
@@ -20,6 +24,10 @@ const config: Config = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    iconsPlugin({
+      collections: getIconCollections(["tabler","mdi"])
+    })
+  ],
 };
 export default config;
