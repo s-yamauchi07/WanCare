13826ece03c04660fdcf3f204965557c43c83fa2
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-05 22:40:06 +0900

    コメント更新のAPI作成

--- src/app/api/diaries/[id]/comments/[commentId]/route.ts
+++ src/app/api/diaries/[id]/comments/[commentId]/route.ts
@@ -27,3 +27,31 @@ export const DELETE = async(request: NextRequest, { params } : { params: { id: s
     return handleError(error);
   }
 }
+
+// コメント編集
+export const PUT = async(request: NextRequest, {params}: { params: { id: string }} ) => {
+  const { id } = params
+  const { data, error } = await userAuthentication(request);
+  if (error) return handleError(request);
+  
+  const currentUserId = data.user.id;
+  await verifyUser(currentUserId, id, prisma.comment)
+
+  const body = await request.json();
+  const { comment } = body;
+
+  try {
+    const updatedComment = await prisma.comment.update({
+      where: {
+        id,
+      },
+      data: {
+        comment,
+      },
+    });
+
+    return NextResponse.json({ status: "OK", message: "コメントを更新しました", comment: updatedComment });
+  } catch(error) {
+    return handleError(error);
+  }
+}
