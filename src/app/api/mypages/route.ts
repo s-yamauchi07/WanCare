8cb4974d675c475d7fefc59f41a5c309ca6c5a13
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-06 00:48:54 +0900

    マイページ実装

---
+++ src/app/api/mypages/route.ts
@@ -0,0 +1,69 @@
+import { userAuthentication } from "@/app/utils/userAuthentication";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { handleError } from "@/app/utils/errorHandler";
+
+const prisma = new PrismaClient();
+
+// mypage表示
+export const GET = async(request: NextRequest) => {
+  const { data, error } = await userAuthentication(request);
+  if (error) return handleError(request);
+
+  const currentUserId = data.user.id;
+
+  try {
+    const userInfo = await prisma.owner.findUnique({
+      where: {
+        id: currentUserId,
+      },
+      include: {
+        dog: {
+          select: {
+            name: true,
+            sex: true,
+            birthDate: true,
+          },
+        },
+        diaries: {
+          select: {
+            title: true,
+            imageKey: true,
+            createdAt: true,
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+        },
+        summaries: {
+          select: {
+            title: true,
+            createdAt: true,
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+        },
+        bookmarks: {
+          select: {
+            diary: {
+              select: {
+                title: true,
+                imageKey: true,
+                createdAt: true,
+              },
+            },
+          },
+        },
+      },
+    })
+
+    if(!userInfo) {
+      return NextResponse.json({ status: "Not found" , message: "User information Not found."}, { status: 404});
+    }
+
+    return NextResponse.json( { status: "OK", userInfo: userInfo }, { status: 200 });
+  } catch(error) {
+    return handleError(error);
+  }
+}
