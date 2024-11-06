4ece99131f9ee5d6737efe201b6cf4588e1daa40
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-07 01:15:10 +0900

    まとめ編集のAPI作成

--- src/app/api/summaries/[id]/route.ts
+++ src/app/api/summaries/[id]/route.ts
@@ -2,6 +2,9 @@ import { handleError } from "@/app/utils/errorHandler";
 import { userAuthentication } from "@/app/utils/userAuthentication";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import { Summary } from "@/_types/summary";
+import { verifyUser } from "@/app/utils/verifyUser";
+import { findOrCreateTag } from "@/app/utils/findOrCreateTag";
 
 const prisma = new PrismaClient();
 
@@ -40,6 +43,84 @@ export const GET = async(request: NextRequest, {params} : { params: { id: string
     }
 
     return NextResponse.json({ status: "OK", summary: summary }, { status: 200});
+  } catch(error) {
+    return handleError(error);
+  }
+}
+
+// 編集
+export const PUT = async(request: NextRequest, { params } : { params : { id: string }} ) => {
+  const { id } = params
+  const { data, error } = await userAuthentication(request);
+  if ( error ) return handleError(error);
+
+  const currentUserId = data.user.id;
+  await verifyUser(currentUserId, id, prisma.summary);
+
+  const body = await request.json();
+  const { title, explanation, tags, diaryIds } : Summary = body;
+  
+  try {
+    const updateSummary = await prisma.$transaction(async(prisma) => {
+      const summary = await prisma.summary.update({
+        where: {
+          id,
+        },
+        data: {
+          title,
+          explanation,
+        },
+      });
+
+      await prisma.summaryTag.deleteMany({
+        where: {
+          summaryId: id,
+        },
+      });
+
+      if (tags && tags.length > 0) {
+        const updateTag = tags.map(async(tag) => {
+          const addTag = await findOrCreateTag(tag);
+
+          return prisma.summaryTag.create({
+            data: {
+              summaryId: summary.id,
+              tagId: addTag.id
+            },
+          });
+        })
+
+        await Promise.all(updateTag);
+      }
+
+      if (diaryIds && diaryIds.length > 0) {
+        const updateSummary = diaryIds.map(async(id) => {
+          const diary = await prisma.diary.findUnique({
+            where: {
+              id,
+            },
+          });
+
+          if(!diary) {
+            return NextResponse.json({ status: "Not found", message: "diary Not found"}, { status: 404});
+          }
+
+          await prisma.diary.update({
+            where: {
+              id: diary.id,
+            },
+            data: {
+              summaryId: summary.id,
+            },
+          });
+        });
+        await Promise.all(updateSummary);
+      }
+
+      return summary;
+    });
+
+    return NextResponse.json({ status: "OK", summary: updateSummary}, { status: 200 });
   } catch(error) {
     return handleError(error);
   }
