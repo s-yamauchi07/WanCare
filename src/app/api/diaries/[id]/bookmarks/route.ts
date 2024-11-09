32db1092ae7f82b441ab769529961fdc3946c3a1
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-09 11:36:42 +0900

    bookmark削除機能

--- src/app/api/diaries/[id]/bookmarks/route.ts
+++ src/app/api/diaries/[id]/bookmarks/route.ts
@@ -26,3 +26,35 @@ export const POST = async(request: NextRequest, { params } : { params : { id: st
     return handleError(error);
   }
 }; 
+
+// ブックマーク削除
+export const DELETE = async(request: NextRequest, { params } : { params : { id: string} }) => {
+  const { id } = params
+  const { data, error } = await userAuthentication(request);
+  if (error) return handleError(request);
+
+  const currentUserId = data.user.id;
+
+  try {
+    const deleteBookmark = await prisma.bookMark.findFirst({
+      where: {
+        diaryId: id,
+        ownerId: currentUserId,
+      },
+    });
+
+    if(!deleteBookmark) {
+      return NextResponse.json({ status: "Not found.", message: "ブックマークデータが確認できませんでした"}, { status: 404});
+    }
+
+    await prisma.bookMark.delete({
+      where: {
+        id: deleteBookmark.id,
+      },
+    });
+
+    return NextResponse.json({ status: "OK", message: "ブックマークを解除しました"}, { status: 200 });
+  } catch(error) {
+    return handleError(error);
+  }
+}
