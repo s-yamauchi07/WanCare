624139af35c42a1975e727784888711d3d793c3c
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-10 23:55:25 +0900

    Promise.allを使用した並列処理に変更

--- src/app/api/homes/route.ts
+++ src/app/api/homes/route.ts
@@ -7,65 +7,81 @@ const prisma = new PrismaClient();
 
 export const GET = async(request: NextRequest) => {
   const { data, error } = await userAuthentication(request);
-  if (error) return handleError(request);
+  if (error) return handleError(error);
 
   const currentUserId = data.user.id;
   const today = new Date();
   today.setHours(0,0,0,0);
 
   try {
-    const dogInfo = await prisma.owner.findUnique({
-      where: {
-        id: currentUserId,
-      },
-      include: {
-        // ログインユーザーに紐づく犬情報を取得
-        dog: {
-          select: {
-            name: true,
-            sex: true,
-            birthDate: true,
-            adoptionDate: true,
-            imageKey: true,
-            breed: {
-              select: {
-                name: true,
+    const [dogInfo, todayCare, dogWeight] = await Promise.all([
+      prisma.owner.findUnique({
+        where: {
+          id: currentUserId,
+        },
+        include: {
+          // ログインユーザーに紐づく犬情報を取得
+          dog: {
+            select: {
+              name: true,
+              sex: true,
+              birthDate: true,
+              adoptionDate: true,
+              imageKey: true,
+              breed: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+          cares: {
+            select: {
+              careDate: true,
+              amount: true,
+              memo: true,
+              careList: {
+                select: {
+                  name: true,
+                },
               },
             },
           },
         },
-        cares: {
-          where: {
-            careDate: {
-              gte: today,
-              lt: new Date(today.getTime() + 24 * 60 * 60 * 1000),
-            },
+      }),
+      
+      prisma.care.findMany({
+        where: {
+          ownerId: currentUserId,
+          careDate: {
+            gte: today,
+            lt: new Date(today.getTime() + 24 * 60 * 60 * 1000),
           },
         },
-      },
-    });
-
+      }),
+      
+      prisma.care.findMany({
+        where: {
+          ownerId: currentUserId,
+          careList: {
+            name: "体重",
+          },
+        },
+        select: {
+          careDate: true,
+          amount: true,
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      }),
+    ]);
+    
     if (!dogInfo) {
       return NextResponse.json({ status: "Not found", message: "Dog's information does not found."}, { status: 404});
     }
 
-    const dogWeight = await prisma.care.findMany({
-      where: {
-        ownerId: currentUserId,
-        careList: {
-          name: "体重",
-        },
-      },
-      select: {
-        careDate: true,
-        amount: true,
-      },
-      orderBy: {
-        createdAt: "asc",
-      },
-    });
-
-    return NextResponse.json({ status: "OK", dogInfo: dogInfo, dogWeight: dogWeight }, { status: 200 });
+    return NextResponse.json({ status: "OK", dogInfo: dogInfo, todayCare: todayCare, dogWeight: dogWeight }, { status: 200 });
   } catch(error) {
     return handleError(error);
   }
