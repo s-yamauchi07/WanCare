6fd3ac52f4f1d3dff16896d3776ad445eeed028c
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-14 00:12:55 +0900

    diaryのinterface定義

--- src/app/api/diaries/search/route.ts
+++ src/app/api/diaries/search/route.ts
@@ -4,16 +4,27 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
+interface ResponseDiary {
+  id: string
+  title: string
+  content: string
+  imageKey: string | null
+  ownerId: string
+  summaryId: string | null
+  createdAt: Date
+  updatedAt: Date
+}
+
 export const GET = async(request: NextRequest) => {
-  const { searchParams } = request.nextUrl;
-  const keywords = searchParams.get('keywords')?.split(',').map(keyword => decodeURIComponent(keyword.trim())) || [];
+  const { searchParams  }  = request.nextUrl;
+  const keywords: string[] = searchParams.get('keywords')?.split(',').map(keyword => decodeURIComponent(keyword.trim())) || [];
 
   if(keywords.length === 0) {
     return NextResponse.json({ status: "No found", message:"タグを入力してください" }, { status: 404});
   }
 
   try {
-    const diaries = await prisma.diary.findMany({
+    const diaries: ResponseDiary[] = await prisma.diary.findMany({
       where: {
         OR: keywords.map(keyword => ({
           diaryTags: {
