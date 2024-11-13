b2955719010284d80bf99419ebe9225a3616ad2b
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-14 00:01:41 +0900

    diariesの検索機能修正

---
+++ src/app/api/diaries/search/route.ts
@@ -0,0 +1,36 @@
+import { handleError } from "@/app/utils/errorHandler";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export const GET = async(request: NextRequest) => {
+  const { searchParams } = request.nextUrl;
+  const keywords = searchParams.get('keywords')?.split(',').map(keyword => decodeURIComponent(keyword.trim())) || [];
+
+  if(keywords.length === 0) {
+    return NextResponse.json({ status: "No found", message:"タグを入力してください" }, { status: 404});
+  }
+
+  try {
+    const diaries = await prisma.diary.findMany({
+      where: {
+        OR: keywords.map(keyword => ({
+          diaryTags: {
+            some: {
+              tag: {
+                name: {
+                  contains: keyword
+                },
+              },
+            },
+          },
+        })),
+      },
+    });
+
+    return NextResponse.json({ status: "OK", diaries: diaries }, { status: 200});
+  } catch(error) {
+    return handleError(error);
+  }
+}
