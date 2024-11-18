4023d50f5bedab5098d79d1a53abd508942e51a9
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-19 01:36:58 +0900

    toastの追加

--- src/app/dogs/form/page.tsx
+++ src/app/dogs/form/page.tsx
@@ -12,6 +12,8 @@ import { useSupabaseSession } from "@/_hooks/useSupabaseSession";
 import { supabase } from "@/app/utils/supabase";
 import { v4 as uuidv4 } from "uuid";
 import { useRouteGuard } from "@/_hooks/useRouteGuard";
+import { toast, Toaster } from "react-hot-toast"
+import { useRouter } from "next/navigation"
 
 const sexSelection = [
   {id: 1, name: "男の子"},
@@ -24,6 +26,7 @@ const DogForm: React.FC = () => {
 
   const { register, handleSubmit, reset, watch, formState: { errors, isSubmitting}} = useForm<Dog>();
   const { token } = useSupabaseSession();
+  const router = useRouter();
   const [breeds, setBreeds] = useState<Breed[]>([]);
   const imageKey = watch("imageKey");
   const [uploadedKey, setUploadedKey] = useState<string | null>(null);
@@ -54,10 +57,9 @@ const DogForm: React.FC = () => {
   useEffect(() => {
     const uploadImage = async () => {
       if (!imageKey || imageKey.length === 0) return;
-
       setUploading(true);
-      const file = imageKey[0];
-                         
+
+      const file = imageKey[0];                   
       const filePath = `private/${uuidv4()}`;
       const { data, error } = await supabase.storage
         .from("profile_img")
@@ -65,8 +67,9 @@ const DogForm: React.FC = () => {
           cacheControl: "3600",
           upsert: false,
         });
+
       if (error) {
-        alert(error.message);
+        toast.error("画像のアップロードに失敗しました");
         setUploading(false);
         return;
       }
@@ -92,13 +95,14 @@ const DogForm: React.FC = () => {
     fetchImage()
   }, [uploadedKey])
 
+  // 新規登録
   const onsubmit: SubmitHandler<Dog> = async(data) => {
     const req = {
       ...data,
       imageKey: uploadedKey
     }
 
-    if(!token) return
+    if(!token) return;
     try {
       const response = await fetch("/api/dogs", {
         headers: {
@@ -110,127 +114,131 @@ const DogForm: React.FC = () => {
       });
   
       if(response.status === 200) {
-        console.log("登録しました")
+        toast.success("登録が完了しました");
+        router.push("/");
       }
       reset(data);
     } catch(error) {
       handleError(error);
+      toast.error("登録に失敗しました");
     }
   }
 
   return(
-    <form onSubmit={handleSubmit(onsubmit)} className="w-80 px-8 pt-6 pb-8 mb-4">
-      <h2 className="text-primary text-center text-2xl font-bold m-14">ペット登録</h2>
-      
+    <>
+      <form onSubmit={handleSubmit(onsubmit)} className="w-80 px-8 pt-6 pb-8 mb-4">
+        <h2 className="text-primary text-center text-2xl font-bold m-14">ペット登録</h2>
+        
+          <div className="mb-6">
+            <div className="rounded-full border border-primary m-auto w-28 h-28 flex items-center justify-center overflow-hidden relative">
+              <label className="w-full h-full flex items-center justify-center">
+              {thumbnailImageUrl ? (
+                  <div 
+                    className="absolute inset-0 bg-cover bg-center pointer-events-none" 
+                    style={{ backgroundImage: `url(${thumbnailImageUrl })` }}>
+                  </div>
+                ) : (
+                  <div className="flex flex-col items-center">
+                    <span className="i-material-symbols-add-a-photo-outline-rounded text-5xl p-3 text-primary"></span>
+                    <span className="text-xs text-primary font-bold">{isUploading ? "uploading..." : "add image"}</span>
+                  </div>
+                )
+              }
+                <input 
+                  type="file" 
+                  className="absolute inset-0 opacity-0 cursor-pointer"
+                  {...register("imageKey",{
+                    required: "プロフィール画像は必須です。",
+                })}
+                />
+              </label>
+            </div>
+            <div className="text-red-500 text-xs text-center mt-2">{errors.imageKey?.message}</div>
+          </div>
+
         <div className="mb-6">
-          <div className="rounded-full border border-primary m-auto w-28 h-28 flex items-center justify-center overflow-hidden relative">
-            <label className="w-full h-full flex items-center justify-center">
-            {thumbnailImageUrl ? (
-                <div 
-                  className="absolute inset-0 bg-cover bg-center pointer-events-none" 
-                  style={{ backgroundImage: `url(${thumbnailImageUrl })` }}>
-                </div>
-              ) : (
-                <div className="flex flex-col items-center">
-                  <span className="i-material-symbols-add-a-photo-outline-rounded text-5xl p-3 text-primary"></span>
-                  <span className="text-xs text-primary font-bold">{isUploading ? "uploading..." : "add image"}</span>
-                </div>
-              )
-            }
-              <input 
-                type="file" 
-                className="absolute inset-0 opacity-0 cursor-pointer"
-                {...register("imageKey",{
-                  required: "プロフィール画像は必須です。",
+          <Label id="性別" />
+          <div className="inline-block relative w-64">
+            <select 
+              className="block appearance-none border border-primary w-full px-3 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              {...register("sex",{
+                validate: value => value !== "" ||"性別を選択してください。"
+              })}>
+              <option value="">性別を選択してください</option>
+              {sexSelection.map((s) => {
+                return(
+                  <option value={s.name} key={s.id}>{s.name}</option>
+                )
               })}
-              />
-            </label>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+            </div>
           </div>
-          <div className="text-red-500 text-xs text-center mt-2">{errors.imageKey?.message}</div>
+          <div className="text-red-500 text-xs mt-2">{errors.sex?.message}</div>
         </div>
 
-
-      <div className="mb-6">
-        <Label id="性別" />
-        <div className="inline-block relative w-64">
-          <select 
-            className="block appearance-none border border-primary w-full px-3 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-            {...register("sex",{
-              validate: value => value !== "" ||"性別を選択してください。"
-            })}>
-            <option value="">性別を選択してください</option>
-            {sexSelection.map((s) => {
-              return(
-                <option value={s.name} key={s.id}>{s.name}</option>
-              )
-            })}
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        <div className="mb-6">
+          <Label id="犬種" />
+          <div className="inline-block relative w-64">
+            <select 
+              className="block appearance-none border border-primary w-full px-3 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              {...register("breedId",{
+                validate: value => value !== "" ||"犬種を選択してください。"
+              })}>
+              <option value="">犬種を選択してください</option>
+              {breeds.map((breed) => {
+                return (
+                  <option key={breed.id} value={breed.id}>{breed.name}</option>
+                )
+              })}
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+            </div>
           </div>
+          <div className="text-red-500 text-xs mt-2">{errors.breedId?.message}</div>
         </div>
-        <div className="text-red-500 text-xs mt-2">{errors.sex?.message}</div>
-      </div>
 
-      <div className="mb-6">
-        <Label id="犬種" />
-        <div className="inline-block relative w-64">
-          <select 
-            className="block appearance-none border border-primary w-full px-3 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-            {...register("breedId",{
-              validate: value => value !== "" ||"犬種を選択してください。"
-            })}>
-            <option value="">犬種を選択してください</option>
-            {breeds.map((breed) => {
-              return (
-                <option key={breed.id} value={breed.id}>{breed.name}</option>
-              )
-            })}
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-          </div>
-        </div>
-        <div className="text-red-500 text-xs mt-2">{errors.breedId?.message}</div>
-      </div>
+        <Input
+          id="name"
+          labelName="お名前"
+          type="text"
+          placeholder="ぽち"
+          register={{...register("name", {
+            required: "お名前は必須です。"
+          })}}
+          error={errors.name?.message}
+        />
 
-      <Input
-        id="name"
-        labelName="お名前"
-        type="text"
-        placeholder="ぽち"
-        register={{...register("name", {
-          required: "お名前は必須です。"
-        })}}
-        error={errors.name?.message}
-      />
+        <Input
+          id="birthDate"
+          labelName="誕生日"
+          type="date"
+          placeholder="2020/01/01"
+          register={{...register("birthDate", {
+            required: "誕生日は必須です。"
+          })}}
+          error={errors.birthDate?.message}
+        />
 
-      <Input
-        id="birthDate"
-        labelName="誕生日"
-        type="date"
-        placeholder="2020/01/01"
-        register={{...register("birthDate", {
-          required: "誕生日は必須です。"
-        })}}
-        error={errors.birthDate?.message}
-      />
-
-      <Input
-        id="adoptionDate"
-        labelName="おうち記念日"
-        type="date"
-        placeholder="2020/01/01"
-        register={{...register("adoptionDate", {
-          required: "誕生日は必須です。"
-        })}}
-        error={errors.adoptionDate?.message}
-      />
-      <LoadingButton 
-        isSubmitting={isSubmitting}
-        buttonText="登録"
-      />
-    </form>
+        <Input
+          id="adoptionDate"
+          labelName="おうち記念日"
+          type="date"
+          placeholder="2020/01/01"
+          register={{...register("adoptionDate", {
+            required: "誕生日は必須です。"
+          })}}
+          error={errors.adoptionDate?.message}
+        />
+        <LoadingButton 
+          isSubmitting={isSubmitting}
+          buttonText="登録"
+        />
+      </form>
+      <Toaster />
+    </>
   )
 }
 
