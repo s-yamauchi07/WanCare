99a7920f443675f2d6317ebdfd4f9bfc784a71a4
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-19 01:26:00 +0900

    ペット登録画面のUI変更

--- src/app/dogs/new/page.tsx
+++ src/app/dogs/new/page.tsx
@@ -5,14 +5,19 @@ import dog_registration from "@/public/dog_registration.png";
 
 const addDog: React.FC = () => {
   return(
-    <div className="w-80 flex flex-col items-center">
-      <Image src={dog_registration} alt="dog_img" />
-      <p className="text-2xl">
+    <div className="w-80 flex flex-col items-center pt-20">
+      <Image 
+        src={dog_registration}
+        alt="dog_img"
+        width={200}
+        priority={true}
+      />
+      <p className="text-xl">
         ペットを登録しましょう
       </p>
       <Link 
         href="/dogs/form"
-        className="bg-primary hover:bg-emerald-500 text-white font-bold py-2 px-4 mt-6 rounded-full focus:outline-none focus:shadow-outline flex items-center"
+        className="bg-primary hover:bg-emerald-500 text-white font-bold py-2 px-4 mt-4 rounded-full focus:outline-none focus:shadow-outline flex items-center"
       >
         <span className="i-material-symbols-add-rounded mr-1"></span>
         <span>ペットを登録する</span>
