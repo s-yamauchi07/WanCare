ce996c65c4fed5dbe1bf0959604857fd2c1106f0
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-19 00:53:11 +0900

    全画面表示に修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex justify-center`}>
-          <div className="w-main bg-main flex justify-center">
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex justify-center min-h-screen`}>
+          <div className="w-main min-h-screen bg-main flex justify-center">
             {children}
           </div>
       </body>
