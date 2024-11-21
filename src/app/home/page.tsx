15435f135b5e941d3306480556fa04a328671250
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-21 09:40:42 +0900

    ペット情報の表示

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,49 +4,62 @@ import { useRouteGuard } from "@/_hooks/useRouteGuard";
 import { useSupabaseSession } from "@/_hooks/useSupabaseSession";
 import React, { useState } from "react";
 import Menu from "@/app/_components/Menu";
-// import Image from "next/image";
 import { useEffect } from "react";
 import { toast, Toaster } from "react-hot-toast"
+import Image from "next/image";
+import { supabase } from "../utils/supabase";
 
-// interface DogInfo {
-//   id: string;
-//   nickname: string;
-//   createdAt: string;
-//   updatedAt: string;
-//   dog: {
-//     name: string;
-//     sex: string;
-//     birthDate: string;
-//     adoptionDate: string;
-//     imageKey: string;
-//     breed: { name: string }
-//   };
-// }
+interface DogInfo {
+  id: string;
+  nickname: string;
+  createdAt: string;
+  updatedAt: string;
+  dog: {
+    name: string;
+    sex: string;
+    birthDate: string;
+    adoptionDate: string;
+    imageKey: string;
+    breed: { name: string }
+  };
+  cares: {
+    careDate: string;
+    amount: number;
+    memo: string;
+    careList: { name: string }
+  }[]
+}
 
-// interface CareInfo {
-//   careDate: string;
-//   amount: number;
-//   memo: string;
-//   careList: { name: string }
-// }
+interface TodayCareInfo {
+  id: string;
+  careDate: string;
+  amount: number;
+  memo: string;
+  imageKey: string;
+  ownerId: string;
+  careListId: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
-// interface WeightInfo {
-//   careDate: string;
-//   amount: number;
-// }
+interface WeightInfo {
+  careDate: string;
+  amount: number;
+} 
 
 const Home: React.FC = () => {
   useRouteGuard();
   
   const { token } = useSupabaseSession();
-  const [dogInfo, setDogInfo] = useState(null);
-  const [tadayCare, setTodayCare] = useState(null);
-  const [dogWeight, setDogWeignt] = useState(null);
+  const [dogInfo, setDogInfo] = useState<DogInfo | null>(null);
+  const [tadayCare, setTodayCare] = useState<TodayCareInfo[]>([]);
+  const [dogWeight, setDogWeignt] = useState<WeightInfo[]>([]);
+  const [dogImage, setDogImage] = useState("");
   
   useEffect(() => {  
     if(!token) return;
 
-    const fecthUserInfo = async() => {
+    const fecthDogInfo = async() => {
       try {
         const response = await fetch("api/home", {
           headers: {
@@ -61,7 +74,6 @@ const Home: React.FC = () => {
         }
 
         const {dogInfo, todayCare, dogWeight} = await response.json();
-        console.log(dogInfo)
 
         setDogInfo(dogInfo);
         setTodayCare(todayCare);
@@ -70,18 +82,36 @@ const Home: React.FC = () => {
         console.log(error);
       }
     }
-    fecthUserInfo();
+    fecthDogInfo();
   }, [token]);
+
+  useEffect(()=>{
+    const dogImg = dogInfo?.dog.imageKey;
+    if(!dogImg) return;
+
+    const fecthImage = async() => {
+      const { data: { publicUrl}, } = await supabase.storage.from("profile_img").getPublicUrl(dogImg)
+      setDogImage(publicUrl);
+    }
+    fecthImage();
+
+  }, [dogInfo]);
   
   return(
     <>
-     {dogInfo && (
+    {/* 犬の情報 */}
+     {(dogInfo && dogImage) &&(
       <div>
         <div>
-          <span>{dogInfo.dog.name}</span>
+          <span>お名前：{dogInfo.dog.name}</span>
+          <Image src={dogImage} alt="profile_image" width={200} height={200}/>
+          <span>誕生日：{dogInfo.dog.birthDate}</span>
+          <span>おうち記念日：{dogInfo.dog.adoptionDate}</span>
+          <span>犬種: {dogInfo.dog.breed.name}</span>
         </div>
       </div>
      )}
+
       <Toaster />
       <Menu />
     </>
