316bd21b48d1efca3873a5b416149596476e1933
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-30 18:45:37 +0900

    編集用のコンポーネント作成

--- src/app/dogs/edit/page.tsx
+++ src/app/dogs/edit/page.tsx
@@ -1,30 +1,49 @@
-import Image from "next/image";
-import Link from "next/link";
-import dog_registration from "@/public/dog_registration.png";
-import IconButton from "@/app/_components/IconButton";
+// import Image from "next/image";
+// import Link from "next/link";
+// import dog_registration from "@/public/dog_registration.png";
+// import IconButton from "@/app/_components/IconButton";
+"use client"
 
-const addDog: React.FC = () => {
+import DogForm from "@/app/_components/DogForm";
+import { useEffect, useState } from "react"
+import { DogResponse } from "@/_types/dog";
+
+const EditDog: React.FC = () => {
+  const [userId, setUserId] = useState<string | null>(null); 
+  const [dogInfo, setDogInfo] = useState<DogResponse | undefined>(undefined);
+
+  useEffect(()=> {
+    const storedUserId = localStorage.getItem("userId");
+    if(storedUserId) {
+      setUserId(storedUserId)
+    }
+  },[userId])
+
+  useEffect(() => {
+    if(userId) {
+      const fetchDogData = async() => {
+        try {
+          const response = await fetch("/api/dogs/checkDog", {
+            method: 'POST',
+            headers: {
+              "Content-Type" : "application/json",
+            },
+            body: JSON.stringify({userId})
+          })
+          const { dog } = await response.json();
+          console.log(dog)
+          setDogInfo(dog);
+        } catch (error) {
+          console.log("ペット情報取得に失敗しました",error)
+        }
+      }
+      fetchDogData();
+    }
+  },[userId])
+
   return(
-    <div className="flex justify-center">
-      <div className="w-64 my-20 flex flex-col items-center">
-        <Image 
-          src={dog_registration}
-          alt="dog_img"
-          width={200}
-          priority={true}
-        />
-        <p className="text-xl mb-4">
-          ペットを登録しましょう
-        </p>
-        <Link href="/dogs/form">
-          <IconButton 
-            iconName="i-material-symbols-add-rounded"
-            buttonText="ペットを登録する"
-          />
-        </Link>
-      </div>
-    </div>
+    <DogForm isEdit={true} dogInfo={dogInfo}/>
   )
 }
 
-export default addDog;
+export default EditDog;
