e0ecf05b8ab5283cf517e6b052edf797a0bf6e16
s-yamauchi07 <hand1nhand306@gmail.com>
2024-12-01 00:18:37 +0900

    新規投稿用のコンポーネント作成

--- src/app/dogs/registration/page.tsx
+++ src/app/dogs/registration/page.tsx
@@ -1,25 +1,6 @@
 "use client"
 
-// import Input from "@/app/_components/Input";
-// import { DogRequest } from "@/_types/dog";
-// import { Breed } from "@/_types/breed";
-// import { useForm, SubmitHandler } from "react-hook-form";
-// import Label from "@/app/_components/Label";
-// import { useEffect, useState } from "react";
-// import LoadingButton from "@/app/_components/LoadingButton";
-// import { useSupabaseSession } from "@/_hooks/useSupabaseSession";
-// import { supabase } from "@/app/utils/supabase";
-// import { v4 as uuidv4 } from "uuid";
-// import { useRouteGuard } from "@/_hooks/useRouteGuard";
-// import { toast, Toaster } from "react-hot-toast"
-// import { useRouter } from "next/navigation"
 import DogForm from "@/app/_components/DogForm";
-
-// const sexSelection = [
-//   {id: 1, name: "男の子"},
-//   {id: 2, name: "女の子"},
-//   {id: 3, name: "不明"}
-// ]
 
 const DogRegistration: React.FC = () => {
   return(
