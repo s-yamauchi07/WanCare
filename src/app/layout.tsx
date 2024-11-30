029f0f2228bfc92bcbf089e69b0f463f5d0401e0
s-yamauchi07 <hand1nhand306@gmail.com>
2024-11-30 11:31:48 +0900

    googleFontの適用

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import localFont from "next/font/local";
+import { Sawarabi_Gothic } from "next/font/google";
 import Menu from "@/app/_components/Menu";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const SawarabiGothic = Sawarabi_Gothic({
+  weight: "400",
+  subsets: ["latin"]
+}) 
 
 export const metadata: Metadata = {
   title: "WanCare",
@@ -27,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex justify-center`}>
+        className={`${SawarabiGothic.className} antialiased min-h-screen flex justify-center`}>
           <div className="w-main min-h-full bg-main">
             {children}
           </div>
