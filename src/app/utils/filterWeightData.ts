f6e93e20756c9ff4be80de319b08eb4cad653798
s-yamauchi07 <hand1nhand306@gmail.com>
2024-12-01 17:25:05 +0900

    体重変動のロジック修正

--- src/app/utils/filterWeightData.ts
+++ src/app/utils/filterWeightData.ts
@@ -10,14 +10,14 @@ const filterLastWeek = (data: WeightInfo[]) : WeightInfo[] => {
 const filterLastMonth = (data: WeightInfo[]) : WeightInfo[] => {
   const today = new Date();
   const lastMonth = new Date(today);
-  lastMonth.setDate(today.getMonth() - 1);
+  lastMonth.setMonth(today.getMonth() - 1);
   return data.filter(d => new Date(d.careDate) >= lastMonth);
 };
 
 const filterLastThreeMonths = (data: WeightInfo[]) : WeightInfo[] => {
   const today = new Date();
   const lastThreeMonths = new Date(today);
-  lastThreeMonths.setDate(today.getMonth() - 3);
+  lastThreeMonths.setMonth(today.getMonth() - 3);
   return data.filter(d => new Date(d.careDate) >= lastThreeMonths);
 };
 
