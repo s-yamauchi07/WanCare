aeb54d82a5ae853aa81cbac9e2b4c6ad06c13056
s-yamauchi07 <hand1nhand306@gmail.com>
2024-12-01 18:58:42 +0900

    Icon表示のCSS変更

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -154,7 +154,7 @@ const Home: React.FC = () => {
                   return(
                     <li key={care.id} className="border rounded-full py-2 px-4 shadow-md">
                       <div className="flex gap-2">
-                        <span className={`i-${care.careList.icon} w-5 h-5`}></span>
+                        <span className={`${care.careList.icon} w-5 h-5`}></span>
                         <span className="w-24">{care.careList.name}</span>
                         <span>{changeTimeFormat(care.careDate)}</span>
                       </div>
