cb534be55240c38d15582b808eb28b9c37882da9
s-yamauchi07 <hand1nhand306@gmail.com>
2024-12-01 19:19:40 +0900

    careListsのfetchAPI

---
+++ src/app/api/careLists/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { userAuthentication } from "@/app/utils/userAuthentication";
+import { handleError } from "@/app/utils/errorHandler";
+import prisma from "@/libs/prisma";
+
+
+// カテゴリー一覧の取得
+export const GET = async (request: NextRequest) => {
+  const { error } = await userAuthentication(request);
+  if (error) return handleError(error);
+
+  try {
+    const careLists = await prisma.careList.findMany({
+      select: {
+        id: true,
+        name: true,
+        icon: true,
+      },
+      orderBy: {
+        order: "asc",
+      },
+    })
+    return NextResponse.json({ status: "OK", cares: careLists }, { status: 200 });
+  } catch(error) {
+    return handleError(error);
+  }
+
+}
