e47b6d07183c1cf5b64ffd8a197214ba4e883bc7
s-yamauchi07 <hand1nhand306@gmail.com>
2024-12-04 23:58:37 +0900

    小数点の入力も許容できるようにpropsの設定

--- src/app/_components/Input.tsx
+++ src/app/_components/Input.tsx
@@ -8,12 +8,13 @@ interface InputProps {
   id: string
   labelName: string
   type: string
+  step?: string
   placeholder: string
   register: UseFormRegisterReturn
   error?: string
 }
 
-const Input: React.FC<InputProps> = ({ id, labelName, type, placeholder, register, error }) => {
+const Input: React.FC<InputProps> = ({ id, labelName, type, placeholder, step, register, error }) => {
   return(
     <div className="mb-6">
       <Label id={labelName}/>
@@ -21,6 +22,7 @@ const Input: React.FC<InputProps> = ({ id, labelName, type, placeholder, registe
         className="appearance-none border border-primary rounded w-full h-10 py-2 px-3 border-primary bg-white text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
         id={id}
         type={type}
+        step={step}
         placeholder={placeholder}
         {...register} 
       />
