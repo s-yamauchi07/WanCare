e2d979f9c600ed9e08a2d1dffe690aac86927873
s-yamauchi07 <hand1nhand306@gmail.com>
2024-12-04 21:51:46 +0900

    dateformatの変更

--- src/app/utils/dateFormat.ts
+++ src/app/utils/dateFormat.ts
@@ -1,2 +1,8 @@
 // 2024-01-01(string)→2024-01-01T15:00:00.000Z(ISO8601形式)
-export const formatDate = (date: string) => new Date(date).toISOString();
+// export const formatDate = (date: string) => new Date(date).toISOString();
+
+export const formatDate = (date: string) => {
+const localDate = new Date(date);
+const utcDate = new Date(localDate.getTime() - (localDate.getTimezoneOffset() * 60000));
+return utcDate.toISOString();
+}
