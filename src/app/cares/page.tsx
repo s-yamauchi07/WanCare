b64793c7cc018c81adf01b523ac6843c599a24d4
s-yamauchi07 <hand1nhand306@gmail.com>
2024-12-08 17:18:38 +0900

    お世話記録一覧のcomponents作成

---
+++ src/app/cares/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+
+const careIndex: React.FC = () => {
+  return(
+    <div className="flex justify-center">
+      <div className="max-w-64 my-20 flex flex-col items-center">
+      <h2 className="text-primary text-center text-2xl font-bold mb-10">お世話ログ</h2>
+      {/* カレンダー表示 */}
+      <div>
+
+      </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default careIndex;
