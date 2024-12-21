303d71685cd741976c7f517240079819a0ccdb23
s-yamauchi07 <hand1nhand306@gmail.com>
2024-12-21 13:16:13 +0900

    プレビュー機能の実装

--- src/app/diaries/new/page.tsx
+++ src/app/diaries/new/page.tsx
@@ -4,14 +4,15 @@ import React, { useEffect, useState } from "react";
 import Input from "@/app/_components/Input";
 import Textarea from "@/app/_components/Textarea";
 import { FileInput, Label } from "flowbite-react";
+import LoadingButton from "@/app/_components/LoadingButton";
+import { toast, Toaster } from "react-hot-toast";
+import PageLoading from "@/app/_components/PageLoading";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useRouteGuard } from "@/_hooks/useRouteGuard";
-import { DiaryRequest } from "@/_types/diary";
 import { useEditPreviewImage } from "@/_hooks/useEditPreviewImage";
 import useUploadImage from "@/_hooks/useUploadImage";
 import { useSupabaseSession } from "@/_hooks/useSupabaseSession";
-import LoadingButton from "@/app/_components/LoadingButton";
-import { toast, Toaster } from "react-hot-toast";
+import { DiaryRequest } from "@/_types/diary";
 
 interface SummaryResponse {
   id: string;
@@ -25,6 +26,7 @@ const AddDiary: React.FC<DiaryRequest> = () => {
   const userId = session?.user.id;
   const [summaryLists, setSummaryLists] = useState<SummaryResponse[]>([]);
   const [isLoading, setLoading] = useState<boolean>(true);
+
 
   const { register, handleSubmit, reset, watch, formState: { errors, isSubmitting}} = useForm<DiaryRequest>();
   const imageKey = watch("imageKey");
@@ -87,95 +89,101 @@ const AddDiary: React.FC<DiaryRequest> = () => {
   }
 
   return(
-    <div className="flex justify-center">
-      <form className="max-w-64 my-20 pb-20" onSubmit={handleSubmit(onSubmit)}>
-        <h2 className="text-primary text-center text-2xl font-bold mb-10">日記投稿</h2>
+    <>
+      {!isLoading ? (
+        <div className="flex justify-center">
+          <form className="max-w-64 my-20 pb-20" onSubmit={handleSubmit(onSubmit)}>
+            <h2 className="text-primary text-center text-2xl font-bold mb-10">日記投稿</h2>
 
-        <Input 
-          id="title"
-          labelName="タイトル"
-          type="text"
-          placeholder="アレルギーの検査"
-          register={{...register("title", {
-            required: "タイトルは必須です。"
-          })}}
-          error={errors.title?.message}
-        />
-
-        <Input 
-          id="tags"
-          labelName="タグ"
-          type="text"
-          placeholder="柴犬 アレルギー"
-          register={{...register("tags")}}
-          error={errors.tags?.message}
-        />
-
-        <Textarea 
-          id="content"
-          labelName="内容"
-          placeholder="散歩の後に足に痒みが出てきた様子。"
-          register={{...register("content",{
-            required: "内容は必須です。"
-          })}}
-          error={errors.content?.message}
-        />
-
-        <div className="flex w-full items-center justify-center relative">
-          <Label
-            htmlFor="dropzone-file"
-            className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-primary bg-gray-50 hover:bg-gray-100"
-          >
-            <div className="flex flex-col items-center justify-center pb-6 pt-5">
-              {thumbnailImageUrl ? (
-                <div 
-                  className="absolute inset-0 h-64 w-full rounded-lg bg-cover bg-center pointer-events-none"
-                  style={{ backgroundImage: `url(${thumbnailImageUrl })` }}>
-                </div>
-              ) : (
-                <>
-                  <span className="i-tabler-cloud-upload w-8 h-8"></span>
-                  <p className="mb-2 text-xs text-gray-500">
-                    <span className="font-semibold">{isUploading ? "アップロード中": "画像をアップロード"}</span>
-                  </p>
-                </>
-              )}
-            </div>
-            <FileInput 
-              id="dropzone-file"
-              className="hidden" 
-              {...register("imageKey")} 
+            <Input 
+              id="title"
+              labelName="タイトル"
+              type="text"
+              placeholder="アレルギーの検査"
+              register={{...register("title", {
+                required: "タイトルは必須です。"
+              })}}
+              error={errors.title?.message}
             />
-          </Label>
-        </div>
 
-        <div className="my-6">
-          <label className="block text-primary text-sm font-bold mb-2" id="summaryId">
-            まとめに追加する
-          </label>
-          <div className="inline-block w-64">
-            <select
-              className="block appearance-none border border-primary bg-white text-gray-800 w-full px-3 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-              {...register("summaryId")}
+            <Input 
+              id="tags"
+              labelName="タグ"
+              type="text"
+              placeholder="柴犬 アレルギー"
+              register={{...register("tags")}}
+              error={errors.tags?.message}
+            />
+
+            <Textarea 
+              id="content"
+              labelName="内容"
+              placeholder="散歩の後に足に痒みが出てきた様子。"
+              register={{...register("content",{
+                required: "内容は必須です。"
+              })}}
+              error={errors.content?.message}
+            />
+
+            <div className="flex w-full items-center justify-center relative">
+              <Label
+                htmlFor="dropzone-file"
+                className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-primary bg-gray-50 hover:bg-gray-100"
               >
-              <option value="">追加しない</option>
-              {summaryLists.map((summaryList) => {
-                return(
-                  <option value={summaryList.id} key={summaryList.id}>{summaryList.title}</option>
-                )
-              })} 
-            </select>
-          </div>
-          <div className="text-red-500 text-xs mt-2">{errors.summaryId?.message}</div>
-        </div>
+                <div className="flex flex-col items-center justify-center pb-6 pt-5">
+                  {thumbnailImageUrl ? (
+                    <div 
+                      className="absolute inset-0 h-64 w-full rounded-lg bg-cover bg-center pointer-events-none"
+                      style={{ backgroundImage: `url(${thumbnailImageUrl })` }}>
+                    </div>
+                  ) : (
+                    <>
+                      <span className="i-tabler-cloud-upload w-8 h-8"></span>
+                      <p className="mb-2 text-xs text-gray-500">
+                        <span className="font-semibold">{isUploading ? "アップロード中": "画像をアップロード"}</span>
+                      </p>
+                    </>
+                  )}
+                </div>
+                <FileInput 
+                  id="dropzone-file"
+                  className="hidden" 
+                  {...register("imageKey")} 
+                />
+              </Label>
+            </div>
 
-        <LoadingButton 
-          isSubmitting={isSubmitting}
-          buttonText={"登録"}
-        />
-      </form>
-      <Toaster />
-    </div>
+            <div className="my-6">
+              <label className="block text-primary text-sm font-bold mb-2" id="summaryId">
+                まとめに追加する
+              </label>
+              <div className="inline-block w-64">
+                <select
+                  className="block appearance-none border border-primary bg-white text-gray-800 w-full px-3 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                  {...register("summaryId")}
+                  >
+                  <option value="">追加しない</option>
+                  {summaryLists.map((summaryList) => {
+                    return(
+                      <option value={summaryList.id} key={summaryList.id}>{summaryList.title}</option>
+                    )
+                  })} 
+                </select>
+              </div>
+              <div className="text-red-500 text-xs mt-2">{errors.summaryId?.message}</div>
+            </div>
+
+            <LoadingButton 
+              isSubmitting={isSubmitting}
+              buttonText={"登録"}
+            />
+          </form>
+          <Toaster />
+        </div>
+      ) : (
+        <PageLoading />
+      )}
+    </>
   )
 }
 export default AddDiary;
