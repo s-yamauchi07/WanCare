d100a543bf3a7ae9da7499cf41bede71f8720fdd
s-yamauchi07 <hand1nhand306@gmail.com>
2024-12-21 13:39:37 +0900

    prismaの読み込み記述の修正

--- src/app/utils/findOrCreateTag.ts
+++ src/app/utils/findOrCreateTag.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 
 export const findOrCreateTag = async(tx: Prisma.TransactionClient, tag: string) => {
   const tagName = await tx.tag.upsert({
