f4a4c90dfd03b4a06fe6edaa730c1095677ceafa
s-yamauchi07 <hand1nhand306@gmail.com>
2025-01-11 21:14:06 +0900

    loading数の修正

--- src/app/diaries/_components/LoadingDiary.tsx
+++ src/app/diaries/_components/LoadingDiary.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoadingDiary: React.FC = () => {
-  const skeletons = Array(8).fill(0);
+  const skeletons = Array(6).fill(0);
 
   return(
     <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
