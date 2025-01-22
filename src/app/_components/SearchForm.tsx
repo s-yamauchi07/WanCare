878d983cf6497768b522e3996d32018209b6a50d
s-yamauchi07 <hand1nhand306@gmail.com>
2025-01-22 21:32:29 +0900

    検索ウィンドウのコンポーネント化

---
+++ src/app/_components/SearchForm.tsx
@@ -0,0 +1,18 @@
+const SearchForm: React.FC = () => {
+  return(
+    <form className="max-w-md my-6">   
+    <label id="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+    <div className="relative">
+      <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+        </svg>
+      </div>
+      <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-primary rounded-full bg-gray-50 focus:ring-primary focus:border-primary" required />
+      <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-full text-sm px-4 py-2">検索</button>
+    </div>
+  </form>
+  )
+}
+
+export default SearchForm;
