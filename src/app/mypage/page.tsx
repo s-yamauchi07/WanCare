41bb0e712583981fb07318eb8683e16c20f9b616
s-yamauchi07 <hand1nhand306@gmail.com>
2025-02-01 12:38:09 +0900

    ペットの情報表示エリアを作成

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -6,11 +6,13 @@ import { useSupabaseSession } from "@/_hooks/useSupabaseSession";
 import { toast, Toaster } from "react-hot-toast"
 import Image from "next/image";
 import PageLoading from "../_components/PageLoading";
+import usePreviewImage from "@/_hooks/usePreviewImage";
+import no_registration from "@/public/dog_registration.png";
 
 interface MypageUser {
   id: string;
   nickname: string;
-  dog: { name: string, sex: string, birthDate: string };
+  dog: { name: string, sex: string, birthDate: string, imageKey: string };
   diaries: { title: string, imageKey: string, createdAt: string }[];
   summaries: { title: string, imageKey: string, createdAt: string }[];
   bookmarks: { title: string, createdAt: string}[];
@@ -22,6 +24,7 @@ const MyPage: React.FC<MypageUser> = () => {
   const { token } = useSupabaseSession();
   const [currentUser, setCurrentUser] = useState<MypageUser | null>(null);
   console.log(currentUser)
+  const dogImg = usePreviewImage(currentUser?.dog.imageKey ?? null, "profile_img");
 
   useEffect(() => {
     if(!token) return;
@@ -49,37 +52,81 @@ const MyPage: React.FC<MypageUser> = () => {
     loggedInUser();
   }, [token]);
 
+  const getAgeInMonths = (birthday: string) => {
+    const today = new Date();
+    const birthDate = new Date(birthday);
+    let years = today.getFullYear() - birthDate.getFullYear();
+    let months = today.getMonth() - birthDate.getMonth();
+    
+    if(months < 0) {
+      years--;
+      months +=12;
+    }
+    return `${years}歳${months}ヶ月`
+  }
+
   return(
     <div className="flex justify-center text-gray-800">
       <div className="w-64 my-20 pb-20 flex flex-col gap-10 overflow-y: auto">
         {currentUser ? (
           <>
             <h2 className="text-2xl font-bold text-primary text-center">マイページ</h2>
-            
             {/* user情報 */}
-            <div>
-              <div>
-                {/* <Image 
-                  className="w-28 h-28 rounded-full border border-primary ring-primary ring-offset-2 ring"
-                  src={dogImage} 
-                  alt="profile_image" 
-                  width={112} 
-                  height={112}
-                  style={{objectFit: "cover"}}
-                  priority={true}
-                /> */}
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-col items-center">
+                <span className="i-material-symbols-light-account-circle-outline w-14 h-14"></span>
+                <p className="text-xl font-bold text-center">{currentUser?.nickname}</p>
               </div>
 
-              <div>
-                <p>{currentUser?.nickname}</p>
-                <ul>
-                  <li>投稿</li>
-                  <li>フォロー</li>
-                  <li>フォロワー</li>
-                </ul>
-                <div>プロフィール編集</div>
+              <ul className="flex">
+                <li className="text-center w-1/3">
+                  <p className="font-bold">{currentUser.diaries.length}</p>
+                  <p className="text-xs">投稿</p>
+                </li>
+                <li className="text-center w-1/3">
+                  <p className="font-bold">{currentUser.diaries.length}</p>
+                  <p className="text-xs">フォロー</p>
+                </li>
+                <li className="text-center w-1/3">
+                  <p className="font-bold">{currentUser.diaries.length}</p>
+                  <p className="text-xs">フォロワー</p>
+                </li>
+              </ul>
+
+              <div className="bg-primary rounded-lg text-white flex items-center justify-center py-1">
+                <span className="i-material-symbols-light-edit-square-outline w-5 h-5"></span>
+                <button>プロフィール編集</button>
               </div>
             </div>
+            {/* user情報 */}
+
+            {/* ペット情報 */}
+            <div className="flex flex-col gap-3">
+              <h3 className="text-lg text-primary font-bold">マイペット</h3>
+
+              <div className="flex justify-around">
+                <div>
+                  <Image 
+                    className="w-14 h-14 rounded-full border border-primary ring-primary ring-offset-1 ring"
+                    src={dogImg ? dogImg : no_registration} 
+                    alt="profile_image" 
+                    width={112} 
+                    height={112}
+                    style={{objectFit: "cover"}}
+                    priority={true}
+                  />
+                </div>
+
+                <div>
+                  <p className="text-xl font-bold text-center">{currentUser.dog.name}</p>
+                  <div className="flex gap-2">
+                    <p>{getAgeInMonths(currentUser.dog.birthDate)}</p>
+                    <p>{currentUser.dog.sex}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </>
         ) : (
           <PageLoading />
