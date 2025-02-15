5552861c7be8b4a332970cb845fe4f1744016e02
s-yamauchi07 <hand1nhand306@gmail.com>
2025-02-15 12:18:05 +0900

    タイトルと本文に合致した場合も検索対象にするようにAPI変更

--- src/app/api/diaries/search/route.ts
+++ src/app/api/diaries/search/route.ts
@@ -13,17 +13,31 @@ export const GET = async(request: NextRequest) => {
   try {
     const diaries = await prisma.diary.findMany({
       where: {
-        OR: keywords.map(keyword => ({
-          diaryTags: {
-            some: {
-              tag: {
-                name: {
-                  contains: keyword
+        OR: [
+          ...keywords.map(keyword => ({
+            diaryTags: {
+              some: {
+                tag: {
+                  name: {
+                    contains: keyword
+                  },
                 },
               },
             },
-          },
-        })),
+          })),
+          ...keywords.map(keyword => ({
+            title: {
+              contains: keyword,
+              mode: 'insensitive' as const, // modeの型に合わせるようにinsensitive型であることを明示。
+            }
+          })),
+          ...keywords.map(keyword => ({
+            content: {
+              contains: keyword,
+              mode: 'insensitive' as const,
+            }
+          })),
+        ]
       },
       include: {
         diaryTags: {
