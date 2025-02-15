c5379949303c4b0fa220493977f497566e6f1539
s-yamauchi07 <hand1nhand306@gmail.com>
2025-02-15 22:35:32 +0900

    Keyword検索の型定義

--- src/app/_components/SearchForm.tsx
+++ src/app/_components/SearchForm.tsx
@@ -1,5 +1,6 @@
+"use client"
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import toast, { Toaster } from "react-hot-toast";
 
@@ -13,60 +14,116 @@ interface SearchProps<T> {
   searchType: "diaries" | "summaries";
 }
 
-const SearchForm = <T,>({ token, onSearchResults, searchType }: SearchProps<T>) => {
-  const { register, handleSubmit, reset,} = useForm<SearchValue>();
+interface KeyWordProps {
+  id: string;
+  name: string;
+}
 
-  const onSubmit: SubmitHandler<SearchValue> = async(data) => {
-    const searchWords = data.keywords.split(" ").filter(word => word.trim() !== "") ?? null
-    
-    if(!token) return;
+const SearchForm = <T,>({ token, onSearchResults, searchType }: SearchProps<T>) => {
+  const { register, handleSubmit, reset } = useForm<SearchValue>();
+  const [text, setText] = useState<string>("");
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+  const [suggestions, setSuggestions] = useState<KeyWordProps[]>([]);
+  const [tagLists, setTagLists] = useState<KeyWordProps[]>([]);
+  
+  useEffect(() => {
+    const fetchLists = async() => {
+      if(!token) return;
+      try {
+        const res = await fetch("/api/tags/", {
+          headers: {
+            "Content-Type" : "application/json",
+            Authorization: token,
+          },
+        });
+        const { tags } = await res.json();
+        setTagLists(tags)
+      } catch(error) {
+        console.log(error)
+      }
+    }
+    fetchLists();
+  }, [token]);
+
+  const handleChange = (text: string) => {
+    setText(text);
+    const keywords = text.split(" "); // 入力されたkeywordを半角スペースで分割
+    const lastKeyword = keywords[keywords.length - 1]; // keywordsの配列の最後の要素を取得して変数化。
+    if (lastKeyword.length > 0) {
+      const KeywordsMatch = tagLists.filter((opt) => {
+        const regex = new RegExp(`${lastKeyword}`, "gi");
+        return opt.name.match(regex);
+      });
+      setSuggestions(KeywordsMatch) // キーワードマッチした候補が配列で格納される。
+    } 
+  }
+
+  const onSubmit: SubmitHandler<SearchValue> = async (data) => {
+    const searchWords = data.keywords.split(" ").filter(word => word.trim() !== "") ?? null;
+    if (!token) return;
     try {
       const response = await fetch(`/api/${searchType}/search?keywords=${searchWords}`, {
         headers: {
-          "Content-Type" : "application/json",
+          "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
       });
-
       const { lists } = await response.json();
-      if(lists.length > 0) {
+      if (lists.length > 0) {
         toast.success(`${lists.length}件ヒットしました`)
         onSearchResults(lists);
       } else {
         toast.error("検索結果が見つかりませんでした")
       }
       reset();
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
   }
 
-  return(
+  return (
     <>
-      <form className="max-w-md my-6" onSubmit={handleSubmit(onSubmit)}>   
+      <form className="max-w-md my-6" onSubmit={handleSubmit(onSubmit)}>
         <label id="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
         <div className="relative">
           <div className="absolute inset-y-0 start-0 flex items-center ps-4 pointer-events-none">
             <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
             </svg>
           </div>
-          <input 
+          <input
             type="search"
             id="default-search"
+            value={text}
             className="block w-full p-4 ps-10 text-sm text-gray-900 border border-primary rounded-full bg-gray-50 focus:ring-primary focus:border-primary"
             required
-            {...register("keywords")} 
+            {...register("keywords")}
+            onFocus={() => setIsFocus(true)}
+            onChange={(e) => handleChange(e.target.value)}
           />
-          <button 
+          <button
             type="submit"
             className="text-white absolute end-2.5 bottom-2.5 bg-primary hover:bg-primary focus:ring-2 focus:outline-none font-medium rounded-full text-sm px-4 py-2">検索
           </button>
         </div>
+        {isFocus && (
+          suggestions?.map((suggestion, i) => (
+            <p
+              key={i}
+              onClick={() => {
+                const currentKeywords = text.split(" ");
+                currentKeywords[currentKeywords.length - 1] = suggestion.name; // text(フォーム入力されている配列)の末尾のデータを候補のtextにする
+                setText(currentKeywords.join(" ")); // 入力値を再度スペース区切りの形式に変換
+                setIsFocus(false);
+              }}
+            >
+              {suggestion.name}
+            </p>
+          ))
+        )}
       </form>
       <Toaster />
     </>
   )
 }
-
 export default SearchForm;
