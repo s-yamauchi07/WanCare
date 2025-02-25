a25ac9c5f486d7d60e5a535b9d253d4ebd6396c0
s-yamauchi07 <hand1nhand306@gmail.com>
2025-02-25 23:19:04 +0900

    ChartのUI修正

--- src/app/_components/Chart.tsx
+++ src/app/_components/Chart.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { WeightInfo } from "@/_types/weight";
-import { LineChart, XAxis, YAxis, Tooltip,  CartesianGrid, Line } from "recharts";
+import { LineChart, XAxis, YAxis, Tooltip,  CartesianGrid, Line, ResponsiveContainer } from "recharts";
 import { filterLastWeek, filterLastMonth, filterLastThreeMonths } from "../utils/filterWeightData";
 
 
@@ -39,11 +39,14 @@ const Chart: React.FC<ChartProps> = ({ dogWeight }) => {
 
   // クリックごとに表示する日付を切り替える
   const intervalValue = () => {
+    const dataLength = filteredDate.length;
     switch(period) {
       case 'month':
-        return 7;
+        // 最大7個程度日付表示されるように指定
+        return Math.max(0,Math.floor(dataLength / 7)); 
       case '3month':
-        return 30;
+        // 最大10個程度日付表示されるように指定
+        return Math.max(0, Math.floor(dataLength / 10));
       default:
         return 0; 
     }
@@ -52,22 +55,27 @@ const Chart: React.FC<ChartProps> = ({ dogWeight }) => {
 
   return(
     <>
-      <LineChart width={256} height={200} data={filteredDate}
-        margin={{ top:10, right:20, left:-40, bottom: 0}}>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart 
+          data={filteredDate}
+          margin={{ top:10, right:20, left:-40, bottom: 0}}
+        >
           <XAxis dataKey="careDate" interval={intervalValue()} tick={{fontSize:10}} angle={-30}/>
           <YAxis dataKey="amount" />
           <CartesianGrid strokeDasharray="3 3" />
           <Tooltip />
           <Line type="monotone" dataKey="amount" strokeWidth={3} stroke="#15A083" fill="#15A083" />
-      </LineChart>
+        </LineChart>
+      </ResponsiveContainer>
+      
       <ul className="flex justify-center">
-        <li className="w-1/3 border rounded-lg text-main bg-primary p-1 text-center">
+        <li className={`w-1/3 border border-primary rounded-lg p-1 text-center ${period === 'week' ? 'bg-primary text-main' : 'bg-main text-primary'}`}>
           <button onClick={ () => setPeriod('week')}>1週間</button>
         </li>
-        <li className="w-1/3 border rounded-lg bg-primary text-main p-1 text-center">
+        <li className={`w-1/3 border border-primary rounded-lg p-1 text-center ${period === 'month' ? 'bg-primary text-main' : 'bg-main text-primary'}`}>
           <button onClick={ () => setPeriod('month')}>1ヶ月</button>
         </li>
-        <li className="w-1/3 border rounded-lg bg-primary text-main p-1 text-center">
+        <li className={`w-1/3 border border-primary rounded-lg p-1 text-center ${period === '3month' ? 'bg-primary text-main ' : 'bg-main text-primary'}`}>
           <button onClick={ () => setPeriod('3month')}>3ヶ月</button>
         </li>
       </ul>
