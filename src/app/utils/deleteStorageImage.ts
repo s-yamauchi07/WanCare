80f2d2d35e1eee8b74efbd9a315cabc40c775cc8
s-yamauchi07 <hand1nhand306@gmail.com>
2025-03-05 00:11:03 +0900

    画像削除のメソッド名修正

--- src/app/utils/deleteStorageImage.ts
+++ src/app/utils/deleteStorageImage.ts
@@ -1,6 +1,6 @@
 import { supabase } from "@/app/utils/supabase";
 
-export const useDeleteStorageImage = async (image: string, bucketName: string) => {
+export const deleteStorageImage = async (image: string, bucketName: string) => {
   try {
     const { error } = await supabase.storage.from(bucketName).remove([image]);
     if(error) {
