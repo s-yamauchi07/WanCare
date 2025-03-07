b0bebda68d1173a0e4fd41470847988809ef3b91
s-yamauchi07 <hand1nhand306@gmail.com>
2025-03-08 00:12:18 +0900

    日記のキャッシュ化

--- src/_types/diary.ts
+++ src/_types/diary.ts
@@ -23,7 +23,7 @@ export interface BaseDiary {
 export interface DiaryDetails extends BaseDiary {
   owner: { id: string, nickname: string }
   comments: CommentProps[];
-  bookmarks: { id: string }[];
+  bookmarks: { id: string, ownerId: string }[];
 }
 
 export interface MypageDiaryLists {
