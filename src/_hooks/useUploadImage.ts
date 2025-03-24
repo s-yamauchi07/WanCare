e44edb7b97870be02ff9058ffe1e6723fd9c36ac
s-yamauchi07 <hand1nhand306@gmail.com>
2025-03-24 14:11:29 +0900

    画像圧縮処理の実装

--- src/_hooks/useUploadImage.ts
+++ src/_hooks/useUploadImage.ts
@@ -2,10 +2,16 @@ import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { supabase } from "../app/utils/supabase";
 import { toast } from "react-hot-toast";
+import imageCompression from "browser-image-compression";
 
 export const useUploadImage = (imageKey: string | null, bucketName: string) => {
   const [isUploading, setUploading] = useState<boolean>(false);
   const [uploadedKey, setUploadedKey] = useState<string | null>(null);
+
+  const options = {
+    maxSizeMB: 1,
+    useWebWorker: true, 
+  }
 
   useEffect(()=> {
     const uploadImage = async () => {
@@ -14,21 +20,28 @@ export const useUploadImage = (imageKey: string | null, bucketName: string) => {
       if(typeof imageKey[0] === "object") {
         setUploading(true);
 
-        const file = imageKey[0];                   
+        const file = imageKey[0];
         const filePath = `private/${uuidv4()}`;
-        const { data, error } = await supabase.storage
-          .from(bucketName).upload(filePath, file, {
-            cacheControl: "3600",
-            upsert: false,
-          });
 
-        if (error) {
-          toast.error("画像のアップロードに失敗しました");
+        try {
+          const compressedFile = await imageCompression(file, options);
+          const { data, error } = await supabase.storage
+            .from(bucketName).upload(filePath, compressedFile, {
+              cacheControl: "3600",
+              upsert: false,
+            });
+  
+          if (error) {
+            toast.error("画像のアップロードに失敗しました");
+            setUploading(false);
+            return;
+          }
+          setUploadedKey(data.path);
           setUploading(false);
-          return;
+        } catch(error) {
+          console.log(error);
+          toast.error("画像の圧縮に失敗しました")
         }
-        setUploadedKey(data.path);
-        setUploading(false);
       }
     }
     uploadImage();
