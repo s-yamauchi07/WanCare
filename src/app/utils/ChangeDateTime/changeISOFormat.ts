597b242f135839e7573a201b554b701d055a11b7
s-yamauchi07 <hand1nhand306@gmail.com>
2025-03-31 23:46:35 +0900

    data-fnsライブラリを使った日付変換

--- src/app/utils/ChangeDateTime/changeISOFormat.ts
+++ src/app/utils/ChangeDateTime/changeISOFormat.ts
@@ -1,8 +1,6 @@
+import { startOfDay, formatISO } from "date-fns";
+
 export const getISODateWithMidnightInJST = (targetDate: Date) => {
-  // 日本時間の0:00:00に設定
-  targetDate.setHours(0, 0, 0, 0);
-  // JSTのタイムゾーンオフセット（通常+9時間）
-  const offset = targetDate.getTimezoneOffset() * 60000;
-  const jstDate = new Date(targetDate.getTime() - offset);
-  return jstDate.toISOString();
+  const startOfDayDate = startOfDay(targetDate);
+  return formatISO(startOfDayDate);
 }
