80a70952d612493490ca2d3f311f8109d0de73be
s-yamauchi07 <hand1nhand306@gmail.com>
2025-04-12 02:38:18 +0900

    パスの修正

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,13 +11,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#f5ebda',
     icons: [
       {
-        "src": "/wancare/public/web-app-manifest-192x192.png",
+        "src": "/web-app-manifest-192x192.png",
         "sizes": "192x192",
         "type": "image/png",
         "purpose": "maskable"
       },
       {
-        "src": "/wancare/public/web-app-manifest-512x512.png",
+        "src": "/web-app-manifest-512x512.png",
         "sizes": "512x512",
         "type": "image/png",
         "purpose": "maskable"
